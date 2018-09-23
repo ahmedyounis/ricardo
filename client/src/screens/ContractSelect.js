e5496de906899f933b941c1a4540faa889c5c958
John Worthley <john@Johns-MacBook-Pro-4.local>
2018-09-23 03:37:32 +0100

    asdfg

--- client/src/screens/ContractSelect.js
+++ client/src/screens/ContractSelect.js
@@ -1,7 +1,7 @@
+
 import React, { Component } from 'react';
 import '../App.css';
 import { Link } from 'react-router-dom';
-
 import { withStyles } from '@material-ui/core/styles';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
@@ -9,76 +9,158 @@ import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Button from '@material-ui/core/Button';
+import PropTypes from 'prop-types';
+import ButtonBase from '@material-ui/core/ButtonBase';
 
-import EOSIOClient from '../utils/eosio';
 
 const styles = theme => ({
   root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    minWidth: 300,
     width: '100%',
   },
-  heading: {
-    fontSize: theme.typography.pxToRem(15),
-    fontWeight: theme.typography.fontWeightRegular,
+  image: {
+    position: 'relative',
+    height: 200,
+    [theme.breakpoints.down('xs')]: {
+      width: '100% !important', // Overrides inline-style
+      height: 100,
+    },
+    '&:hover, &$focusVisible': {
+      zIndex: 1,
+      '& $imageBackdrop': {
+        opacity: 0.15,
+      },
+      '& $imageMarked': {
+        opacity: 0,
+      },
+      '& $imageTitle': {
+        border: '4px solid currentColor',
+      },
+    },
   },
-  button: {
-    margin: theme.spacing.unit,
+  focusVisible: {},
+  imageButton: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: theme.palette.common.white,
   },
-  input: {
-    display: 'none',
-  }
+  imageSrc: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center 40%',
+  },
+  imageBackdrop: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundColor: theme.palette.common.black,
+    opacity: 0.4,
+    transition: theme.transitions.create('opacity'),
+  },
+  imageTitle: {
+    position: 'relative',
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 4}px ${theme.spacing.unit + 6}px`,
+  },
+  imageMarked: {
+    height: 3,
+    width: 18,
+    backgroundColor: theme.palette.common.white,
+    position: 'absolute',
+    bottom: -2,
+    left: 'calc(50% - 9px)',
+    transition: theme.transitions.create('opacity'),
+  },
 });
 
-export default class ContractSelect extends Component {
-  render() {
-    return (
-      <div className="container">
-        <div className="title">
-          <div className = "center">
-            <h1>Contracts</h1>
-          </div>
-        </div>
-        <div className="content">
-          <h2>Select a contract</h2>
 
-          <div className={styles.root}>
-            <ExpansionPanel>
-              <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                <Typography className={styles.heading}>Choose a Contract Type</Typography>
-              </ExpansionPanelSummary>
-              <ExpansionPanelDetails>
-                <Typography>
-                  <Link to="/create">
-                    <Button variant="contained" href="#contained-buttons" className={styles.button}>
-                      Marriage
-                    </Button>
-                  </Link><br/><br/>
-                  <Link to="/create">
-                    <Button variant="contained" href="#contained-buttons" className={styles.button}>
-                      Auto Loan
-                    </Button>
-                  </Link><br/><br/>
-                  <Link to="/create">
-                    <Button variant="contained" href="#contained-buttons" className={styles.button}>
-                      Mortgage
-                    </Button>
-                  </Link><br/><br/>
-                  <Link to="/create">
-                    <Button variant="contained" href="#contained-buttons" className={styles.button}>
-                      Separation
-                    </Button>
-                  </Link><br/><br/>
-                  <Link to="/create">
-                    <Button variant="contained" href="#contained-buttons" className={styles.button}>
-                      Last Will
-                    </Button>
-                  </Link>
-                </Typography>
-              </ExpansionPanelDetails>
-            </ExpansionPanel>
-          </div>
+const images = [
+  {
+    url: 'breakfast.jpg',
+    title: <Link to="/Wizard" style={{textDecoration: 'none', color:'white'}}>Marriage </Link>,
+    width: '33%',
+  },
+  {
+    url: 'nut.jpg',
+    title: <Link to="/Wizard" style={{textDecoration: 'none', color:'white'}}>Wizard </Link>,
+    width: '34%',
+  },
+  {
+    url: '/car.jpg',
+    title: <Link to="/Auto Loan" style={{textDecoration: 'none', color:'white'}}>Auto Loan </Link>,
+    width: '33%',
+  },
+  {
+    url: '/gear.jpg',
+    title: <Link to="/Mortgage" style={{textDecoration: 'none', color:'white'}}>Mortgage </Link>,
+    width: '33%',
+  },
+  {
+    url: '/separation.jpg',
+    title: <Link to="/lkmn" style={{textDecoration: 'none', color:'white'}}>Separation </Link>,
+    width: '34%',
+  },
+  {
+    url: '/gear.jpg',
+    title: <Link to="/Last Will" style={{textDecoration: 'none', color:'white'}}>Last Will </Link>,
+    width: '33%',
+  },
+];
 
-        </div>
-      </div>
-    );
-  }
+function ButtonBases(props) {
+  const { classes } = props;
+
+  return (
+    <div className={classes.root}>
+      {images.map(image => (
+        <ButtonBase
+          focusRipple
+          key={image.title}
+          className={classes.image}
+          focusVisibleClassName={classes.focusVisible}
+          style={{
+            width: image.width,
+          }}
+        >
+          <span
+            className={classes.imageSrc}
+            style={{
+              backgroundImage: `url(${image.url})`,
+            }}
+          />
+          <span className={classes.imageBackdrop} />
+          <span className={classes.imageButton}>
+            <Typography
+              component="span"
+              variant="subheading"
+              color="inherit"
+              className={classes.imageTitle}
+            >
+              {image.title}
+              <span className={classes.imageMarked} />
+            </Typography>
+          </span>
+        </ButtonBase>
+      ))}
+    </div>
+  );
 }
+
+ButtonBases.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ButtonBases);
